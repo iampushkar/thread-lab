9ea69b6efc5153178e78ef20efb8ccccbf2817e1
Manish Pushkar <m4manishpushkar@gmail.com>
2025-02-04 18:48:56 +0000

    01- data race condition

---
+++ DataRace/DataRace.java
@@ -0,0 +1,39 @@
+package DataRace;
+
+public class DataRace {
+    public static void main(String[] args) {
+        SharedClass sharedClass = new SharedClass();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.checkForDataRace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    public static class SharedClass {
+        private volatile int x = 0;
+        private volatile int y = 0;
+
+        public void increment() {
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace() {
+            if (y > x) {
+                // this particular if block gets executed when x & y are not volatile - data race condition
+                System.out.println("y > x : Data Race is detected : " + y + " > " + x);
+            }
+        }
+    }
+}
